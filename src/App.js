ebdefcc542f6d3497eb72f93f4940ce1795cd7e7
Parth Sethi <parth.sethi.2015@gmail.com>
2020-08-12 20:08:08 +0530

    Checking working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles.css';
+import Navbar from "./Navbar"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class Navbar extends Component(){
+    constructor(){
+        super()
+    }
+
+    render(){
+        <div>hello world</div>
+    }
+}
+
+export default Navbar
