a837e27dbaf6af9cbd5f050cbd1ba3e5631334d9
tperm94 <67191889+tperm94@users.noreply.github.com>
2020-08-14 00:36:28 +0530

    IST

--- utils/messages.js
+++ utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().add('300', 'minutes').format("h:mm a"),
+    time: moment().add('350', 'minutes').format("h:mm a"),
   };
 }
 
